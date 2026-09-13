e4a41d1335a8d491640bfccf3205078101eb0840
Venray92 <venray99@gmail.com>
2026-09-13 20:58:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -363,8 +363,8 @@ with col_left:
         with st.spinner("Memindai pasar..."):
             df_bull, df_bear = run_screener(SAHAM_LIST)
 
-        # HEIGHT DISESUAIKAN MENJADI 425 PX BIAR PAS SEJAJAR BAWAH CHART
-        with st.container(height=425):
+        # HEIGHT DISESUAIKAN MENJADI 448 PX SUPAYA TEPAT PRESISI DENGAN ALAS CHART TRADINGVIEW
+        with st.container(height=448):
             if st.session_state.watchlist_tab == "bull":
                 if not df_bull.empty:
                     for index, row in df_bull.iterrows():
@@ -415,7 +415,7 @@ with col_right:
 
     st.markdown("<div style='margin-bottom: 2px;'></div>", unsafe_allow_html=True)
 
-    # VIEW 1: TRADINGVIEW CHART (Sejajar sempurna)
+    # VIEW 1: TRADINGVIEW CHART
     if st.session_state.view_mode == "chart":
         tradingview_html = f"""
         <div class="tradingview-widget-container" style="height:535px;width:100%">
